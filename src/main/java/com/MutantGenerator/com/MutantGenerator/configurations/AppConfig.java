d56962e7654e7f344e25c7eb8255c6475c718768
Daniel Ekerot <daniel.ekerot@gmail.com>
2016-07-30 10:23:57 +0200

    Add files via upload

---
+++ src/main/java/com/MutantGenerator/com/MutantGenerator/configurations/AppConfig.java
@@ -0,0 +1,29 @@
+package com.MutantGenerator.configurations;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@EnableConfigurationProperties
+@Configuration
+@EnableAutoConfiguration
+@PropertySource("classpath:application.properties")
+public class AppConfig extends WebMvcAutoConfigurationAdapter{
+
+	@Bean
+	public TemplateResolver templateResolver(){
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCacheable(false);
+		return templateResolver;
+		
+	}
+	
+}
+
